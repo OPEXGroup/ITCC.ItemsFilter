9f0a8238c6ee4fcdd419f5fa48cd95e5ead5b76b
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-02-07 14:04:59 +0300

    PVS-studio comments added

--- ItemsFilter/Model/ViewAttribute.cs
+++ ItemsFilter/Model/ViewAttribute.cs
@@ -1,4 +1,6 @@
-﻿// ****************************************************************************
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+// ****************************************************************************
 // <author>mishkin Ivan</author>
 // <email>Mishkin_Ivan@mail.ru</email>
 // <date>28.01.2015</date>
